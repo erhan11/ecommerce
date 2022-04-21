8b339798ab319d10d44d5c0018c7f72263947dd1
erhan11 <77719565+erhan11@users.noreply.github.com>
2022-04-22 00:41:53 +0300

    navbar fix

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
-import { Search } from "@mui/icons-material";
+import { Search, ShoppingCartOutlined } from "@mui/icons-material";
+import { Badge } from "@mui/material";
 import React from "react";
 import styled from "styled-components";
 
@@ -14,15 +15,18 @@ const Wrapper = styled.div`
 `;
 
 const Left = styled.div`
+  flex: 1;
   display: flex;
   align-items: center;
-  flex: 1;
 `;
 
-const Language = styled.span``;
+const Language = styled.span`
+  font-style: 14px;
+  cursor: pointer;
+`;
 
 const SeachContainer = styled.div`
-  border: 1px solid lightgrey;
+  border: 0.5px solid lightgrey;
   display: flex;
   align-items: center;
   margin-left: 25px;
@@ -30,9 +34,28 @@ const SeachContainer = styled.div`
 `;
 const Center = styled.div`
   flex: 1;
+  text-align: center;
 `;
+
+const Logo = styled.h1`
+  font-weight: bold;
+`;
+
 const Right = styled.div`
   flex: 1;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+`;
+
+const Input = styled.div`
+  border: none;
+`;
+
+const MenuItem = styled.div`
+  font-style: 14px;
+  cursor: pointer;
+  margin-left: 25px;
 `;
 
 const Navbar = () => {
@@ -42,12 +65,22 @@ const Navbar = () => {
         <Left>
           <Language>EN</Language>
           <SeachContainer>
+            <Input />
             <Search />
-            input
           </SeachContainer>
         </Left>
-        <Center>center</Center>
-        <Right>right</Right>
+        <Center>
+          <Logo> Erhan. </Logo>
+        </Center>
+        <Right>
+          <MenuItem>REGISTER</MenuItem>
+          <MenuItem>SIGN IN</MenuItem>
+          <MenuItem>
+            <Badge badgeContent={5} color="primary">
+              <ShoppingCartOutlined />
+            </Badge>
+          </MenuItem>
+        </Right>
       </Wrapper>
     </Container>
   );
