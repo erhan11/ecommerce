c3999993aa3d27199764ffbb89cc7f33018a24d5
erhan11 <77719565+erhan11@users.noreply.github.com>
2022-05-16 17:12:58 +0300

    Working on ProductList

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,12 @@
-import { Facebook, Instagram, Pinterest, Twitter } from "@mui/icons-material";
+import {
+  Email,
+  Facebook,
+  Instagram,
+  Phone,
+  Pinterest,
+  Room,
+  Twitter,
+} from "@mui/icons-material";
 import React from "react";
 import styled from "styled-components";
 
@@ -12,15 +20,59 @@ const Left = styled.div`
   padding: 20px;
 `;
 const Logo = styled.h1``;
-const Desc = styled.p``;
-const SocialContainer = styled.div``;
-const SocialIcon = styled.div``;
+const Desc = styled.p`
+  margin: 20px 0px;
+`;
+const SocialContainer = styled.div`
+  display: flex;
+`;
+const SocialIcon = styled.div`
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  color: white;
+  background-color: #${(props) => props.color};
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-right: 20px;
+`;
 
 const Center = styled.div`
   flex: 1;
+  padding: 20px;
 `;
+
+const Title = styled.h3`
+  margin-bottom: 30px;
+`;
+
+const List = styled.ul`
+  margin: 0px;
+  padding: 0px;
+  list-style: none;
+  display: flex;
+  flex-wrap: wrap;
+`;
+
+const ListItem = styled.li`
+  width: 50%;
+  margin-bottom: 10px;
+`;
+
 const Right = styled.div`
   flex: 1;
+  padding: 20px;
+`;
+
+const ContactItem = styled.div`
+  margin-bottom: 20px;
+  display: flex;
+  align-items: center;
+`;
+
+const Payment = styled.img`
+  width: 50%;
 `;
 
 const Footer = () => {
@@ -30,22 +82,48 @@ const Footer = () => {
         <Logo>Erhan.</Logo>
         <Desc>Descption will be here</Desc>
         <SocialContainer>
-          <SocialIcon>
+          <SocialIcon color="3B5999">
             <Facebook />
           </SocialIcon>
-          <SocialIcon>
+          <SocialIcon color="E4405F">
             <Instagram />
           </SocialIcon>
-          <SocialIcon>
+          <SocialIcon color="55ACEE">
             <Twitter />
           </SocialIcon>
-          <SocialIcon>
+          <SocialIcon color="E60023">
             <Pinterest />
           </SocialIcon>
         </SocialContainer>
       </Left>
-      <Center></Center>
-      <Right></Right>
+      <Center>
+        <Title>Usefull Links</Title>
+        <List>
+          <ListItem>Home</ListItem>
+          <ListItem>Cart</ListItem>
+          <ListItem>Man Fashion</ListItem>
+          <ListItem>Woman Fashion</ListItem>
+          <ListItem>Accessories</ListItem>
+          <ListItem>My Account</ListItem>
+          <ListItem>Order Tracking</ListItem>
+          <ListItem>Wishlist</ListItem>
+          <ListItem>Wishlist</ListItem>
+          <ListItem>Terms</ListItem>
+        </List>
+      </Center>
+      <Right>
+        <Title>Contact</Title>
+        <ContactItem>
+          <Room style={{ marginRight: "10px" }} /> This it the address
+        </ContactItem>
+        <ContactItem>
+          <Phone style={{ marginRight: "10px" }} /> +905338212121
+        </ContactItem>
+        <ContactItem>
+          <Email style={{ marginRight: "10px" }} /> contact@erhan.com
+        </ContactItem>
+        <Payment src="https://i.ibb.co/Qfvn4z6/payment.png" />
+      </Right>
     </Container>
   );
 };
